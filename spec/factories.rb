594e4e0ccac0bba6c012d5ba49b4b616c4a08f8e
Wagner Amaral <wagner@netengine.com.au>
2021-03-10 12:58:20 +1000

    Removed tests.

--- spec/factories.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :user do
-    email { Faker::Internet.email }
-    username { Faker::Internet.username }
-    password { Faker::Internet.password }
-  end
-end
