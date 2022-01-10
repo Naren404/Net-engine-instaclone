c4056f1731dc5a18db9d0882a632dc59ec62f8ff
Naren404 <narendrasunar47@gmail.com>
2022-01-11 10:43:41 +1100

    Initial Setup

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'image_processing', '~> 1.2'
 gem 'devise', '~> 4.7', '>= 4.7.2'
 
 # Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', '>= 1.4.2', require: false
+gem 'bootsnap', '>= 1.9.2', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
-  devise :confirmable, :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: {case_sensitive: false}
